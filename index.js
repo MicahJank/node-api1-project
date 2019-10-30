e14a054a624beedd889c4439617292acad940a67
MicahJank <MicahJank@users.noreply.github.com>
2019-10-30 13:03:39 -0500

    getting the user by id works now

--- index.js
+++ index.js
@@ -23,16 +23,32 @@ server.get('/api/users', (req, res) => {
         });
 });
 
-// server.post('/api/users', (req, res) => {
-//     if(!req.body.name || !req.body.bio) {
-//         res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
-//     } else {
-//         db.insert(req.body)
-//             .then(userID => {
-//                 res.status(201).json(userID);
-//             })
-//             .catch(err => {
-//                 res.status(500).json({ error: "There was an error while saving the user to the database" });
-//             });
-//     };
-// });
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    
+    db.findById(id)
+        .then(user => {
+            if(user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist." });
+            };
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be retrieved."});
+        });
+});
+
+server.post('/api/users', (req, res) => {
+    if(!req.body.name || !req.body.bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    } else {
+        db.insert(req.body)
+            .then(userDoc => {
+                res.status(201).json(userDoc);
+            })
+            .catch(err => {
+                res.status(500).json({ error: "There was an error while saving the user to the database" });
+            });
+    };
+});
