ea03ef05c59aa2fb2cf33cce18e723677acf7ca7
MicahJank <MicahJank@users.noreply.github.com>
2019-10-30 12:03:31 -0500

    added requires for express and the database in index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+
+const server = express();
+
+server.listen(4000, () => {
+    console.log('server listening on port 4000');
+});
+
+// express.json() is a parser function... if json *text* is in the body, this
+// method will parse it into an actual object, so that when we access req.body,
+// it will be an actual object.
+server.use(express.json());
